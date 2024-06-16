087c117bc12474f792f0bead78b7be7f4126698c
aprajitaverma <aprajitavrma@outlook.com>
2024-06-16 21:41:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ if name_on_order:
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 pd_df = my_dataframe.to_pandas()
 st.dataframe(data=pd_df)
 st.stop()
